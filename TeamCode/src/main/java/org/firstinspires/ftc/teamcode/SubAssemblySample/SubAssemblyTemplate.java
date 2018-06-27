61aff395bd2829ba8ff962d7002f219fd8c4efe2
ablauch <32077598+ablauch@users.noreply.github.com>
2018-06-27 19:56:20 -0400

    Added sub assembly sample package

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssemblySample/SubAssemblyTemplate.java
@@ -0,0 +1,48 @@
+package org.firstinspires.ftc.teamcode.SubAssemblySample;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.VoltageSensor;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+
+
+/* Sub Assembly Class
+ */
+public class SubAssemblyTemplate {
+    /* Declare private class object */
+    private Telemetry telemetry = null;         /* local copy of telemetry object from opmode class */
+    private HardwareMap hardwareMap = null;     /* local copy of HardwareMap object from opmode class */
+    private String name = "SubAssemblyName";
+
+    /* Declare public class object */
+    public VoltageSensor BatteryLevel = null;
+
+    /* Subassembly constructor */
+    public SubAssemblyTemplate() {
+    }
+
+    /* Initialization method - to be called before any other methods are used */
+    public void Initialize(LinearOpMode opMode) {
+        /* Set local copies from opmode class */
+        telemetry = opMode.telemetry;
+        hardwareMap = opMode.hardwareMap;
+
+        telemetry.addLine(name + " initialize");
+
+        /* Map hardware devices */
+        BatteryLevel = hardwareMap.voltageSensor.get("Lower hub 3");
+    }
+
+    /* Cleanup method - to be called when done with subassembly to 'turn off' everything */
+    public void Cleanup() {
+        telemetry.addLine(name + " cleanup");
+    }
+
+    /* Subassembly methods */
+    public void Test() {
+        telemetry.addLine(name + " test");
+        telemetry.addData("Battery level = %.1f V", BatteryLevel);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/SubAssemblySample/SubAssemblyTest.java
@@ -0,0 +1,42 @@
+package org.firstinspires.ftc.teamcode.SubAssemblySample;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+
+/* Sub Assembly Test OpMode
+ * This TeleOp OpMode is used to test the functionality of the specific sub assembly
+ */
+// Assign OpMode type (TeleOp or Autonomous), name, and grouping
+@TeleOp(name="SubAssembly Test", group="Test")
+public class SubAssemblyTest extends LinearOpMode {
+
+    SubAssemblyTemplate SubAssembly = new SubAssemblyTemplate();
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        telemetry.addLine("SubAssembly Test OpMode");
+
+        /* Initialize sub-assemblies */
+        SubAssembly.Initialize(this);
+        telemetry.update();
+
+        //waits for that giant PLAY button to be pressed on RC
+        waitForStart();
+
+        //telling the code to run until you press that giant STOP button on RC
+        while (opModeIsActive()) {
+
+            SubAssembly.Test();
+            telemetry.update();
+
+            //let the robot have a little rest, sleep is healthy
+            sleep(40);
+        }
+
+        /* Clean up sub-assemblies */
+        SubAssembly.Cleanup();
+        telemetry.update();
+    }
+}
